8d2bae3601574a0210feba547e2a949269afc126
akeerthika21-byte <a.keerthika21@gmail.com>
2025-10-26 15:59:38 +0530

    Added comment lines

--- bank_app/views.py
+++ bank_app/views.py
@@ -15,30 +15,36 @@ class BankAccountView(APIView):
     permission_classes = [IsAuthenticated]
     throttle_classes = [UserRateThrottle]
     def generate_account_no(self):
+        # Generate a unique 12-digit account number
         while True:
-            acc_no = f"{random.randint(10 ** 11, 10 ** 12 - 1)}"  # 12-digit account number
+            acc_no = f"{random.randint(10 ** 11, 10 ** 12 - 1)}"
             if not BankAccount.objects.filter(Account_no=acc_no).exists():
                 return acc_no
 
     def post(self, request):
+        # Creates a new bank account for an existing customer
         customer_id = request.data.get('customer_id')
         account_type  = request.data.get('account_type').strip().title()
         initial_deposit = request.data.get('initial_deposit')
         branch_name = request.data.get('branch_name').strip()
 
+        # Validate customer_id
         try:
             customer_id = int(customer_id)
         except ValueError:
             return Response({"error": "Invalid customer ID."}, status=400)
 
+        # Validate required fields
         if not all([customer_id, account_type, initial_deposit, branch_name]):
             return Response({'error': 'All fields are required.'}, status=status.HTTP_400_BAD_REQUEST)
 
+        # Validate deposit amount
         try:
             initial_deposit = Decimal(initial_deposit)
         except:
             return Response({'error': 'Invalid deposit amount.'}, status=status.HTTP_400_BAD_REQUEST)
 
+        # Fetch customer
         try:
             customer_obj = Customer.objects.get(Customer_id=customer_id)
         except Customer.DoesNotExist:
@@ -46,17 +52,24 @@ class BankAccountView(APIView):
                 'error': 'Customer not found',
             }
             return Response(msg, status=status.HTTP_404_NOT_FOUND)
+
+        # Validate account type
         if account_type not in ['Savings', 'Current', 'FD']:
             msg = {
                 'error': 'Invalid account type'
             }
             return Response(msg, status=status.HTTP_400_BAD_REQUEST)
+
+        # Validate minimum deposit
         if initial_deposit < Decimal('1000.00'):
             msg = {
                 'error': 'Minimum initial deposit is ₹1000.00.'
             }
             return Response(msg,status=status.HTTP_400_BAD_REQUEST)
+
         account_no = self.generate_account_no()
+
+        # Creates new account
         with transaction.atomic():
             account = BankAccount.objects.create(Account_no=account_no, Customer_id=customer_obj, Account_type=account_type,
                                                  Branch_name=branch_name, Balance=initial_deposit)
